cea7e02c377ff052bac60dc709a7fe9b1fd37f91
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-10-22 19:18:49 +0300

    feat(conf) change *ssl_cert* to arrays and autogenerate ecdsa cert
    ### Summary
    
    Previously you could just set a single value to:
    
    - `ssl_cert` and `ssl_cert_key`
    - `admin_ssl_cert` and `admin_ssl_cert_key`
    - `status_ssl_cert` and `status_ssl_cert_key`
    
    This commit makes them arrays.
    
    Previously we only generated RSA certificate by default which didn't allow
    certain ciphers on default cipher suites. This commit now also generates
    ECDSA certificate by default. On modern and intermediate cipher suites
    the ECDSA certificate is set as the default fallback certificate, on old
    cipher suite the RSA certificate remains the default fallback certificate.
    When specifying the custom certificates, the first specified in array is
    considered the default fallback certificate.

--- kong/conf_loader/init.lua
+++ kong/conf_loader/init.lua
@@ -175,18 +175,24 @@ local PREFIX_PATHS = {
 
   kong_env = {".kong_env"},
 
+  ssl_cert_csr_default = {"ssl", "kong-default.csr"},
   ssl_cert_default = {"ssl", "kong-default.crt"},
   ssl_cert_key_default = {"ssl", "kong-default.key"},
-  ssl_cert_csr_default = {"ssl", "kong-default.csr"},
+  ssl_cert_default_ecdsa = {"ssl", "kong-default-ecdsa.crt"},
+  ssl_cert_key_default_ecdsa = {"ssl", "kong-default-ecdsa.key"},
 
   client_ssl_cert_default = {"ssl", "kong-default.crt"},
   client_ssl_cert_key_default = {"ssl", "kong-default.key"},
 
   admin_ssl_cert_default = {"ssl", "admin-kong-default.crt"},
   admin_ssl_cert_key_default = {"ssl", "admin-kong-default.key"},
+  admin_ssl_cert_default_ecdsa = {"ssl", "admin-kong-default-ecdsa.crt"},
+  admin_ssl_cert_key_default_ecdsa = {"ssl", "admin-kong-default-ecdsa.key"},
 
   status_ssl_cert_default = {"ssl", "status-kong-default.crt"},
   status_ssl_cert_key_default = {"ssl", "status-kong-default.key"},
+  status_ssl_cert_default_ecdsa = {"ssl", "status-kong-default-ecdsa.crt"},
+  status_ssl_cert_key_default_ecdsa = {"ssl", "status-kong-default-ecdsa.key"},
 }
 
 
@@ -270,6 +276,12 @@ local CONF_INFERENCES = {
   status_listen = { typ = "array" },
   stream_listen = { typ = "array" },
   cluster_listen = { typ = "array" },
+  ssl_cert = { typ = "array" },
+  ssl_cert_key = { typ = "array" },
+  admin_ssl_cert = { typ = "array" },
+  admin_ssl_cert_key = { typ = "array" },
+  status_ssl_cert = { typ = "array" },
+  status_ssl_cert_key = { typ = "array" },
   db_update_frequency = {  typ = "number"  },
   db_update_propagation = {  typ = "number"  },
   db_cache_ttl = {  typ = "number"  },
@@ -754,20 +766,42 @@ local function check_and_infer(conf, opts)
     end
   end
 
-  if (concat(conf.proxy_listen, ",") .. " "):find("%sssl[%s,]") then
-    if conf.ssl_cert and not conf.ssl_cert_key then
-      errors[#errors + 1] = "ssl_cert_key must be specified"
-
-    elseif conf.ssl_cert_key and not conf.ssl_cert then
-      errors[#errors + 1] = "ssl_cert must be specified"
+  for _, prefix in ipairs({ "", "admin_", "status_" }) do
+    local listen = conf[(prefix == "" and "proxy_" or prefix) .. "listen"]
+    local ssl_enabled = (concat(listen, ",") .. " "):find("%sssl[%s,]") ~= nil
+    if not ssl_enabled and prefix == "" then
+      ssl_enabled = (concat(conf.stream_listen, ",") .. " "):find("%sssl[%s,]") ~= nil
     end
+    if ssl_enabled then
+      local ssl_cert = conf[prefix .. "ssl_cert"]
+      local ssl_cert_key = conf[prefix .. "ssl_cert_key"]
 
-    if conf.ssl_cert and not pl_path.exists(conf.ssl_cert) then
-      errors[#errors + 1] = "ssl_cert: no such file at " .. conf.ssl_cert
-    end
+      if #ssl_cert > 0 and #ssl_cert_key == 0 then
+        errors[#errors + 1] = prefix .. "ssl_cert_key must be specified"
 
-    if conf.ssl_cert_key and not pl_path.exists(conf.ssl_cert_key) then
-      errors[#errors + 1] = "ssl_cert_key: no such file at " .. conf.ssl_cert_key
+      elseif #ssl_cert_key > 0 and #ssl_cert == 0 then
+        errors[#errors + 1] = prefix .. "ssl_cert must be specified"
+
+      elseif #ssl_cert ~= #ssl_cert_key then
+        errors[#errors + 1] = prefix .. "ssl_cert was specified " .. #ssl_cert .. " times while " ..
+                              prefix .. "ssl_cert_key was specified " .. #ssl_cert_key .. " times"
+      end
+
+      if ssl_cert then
+        for _, cert in ipairs(ssl_cert) do
+          if not pl_path.exists(cert) then
+            errors[#errors + 1] = prefix .. "ssl_cert: no such file at " .. cert
+          end
+        end
+      end
+
+      if ssl_cert_key then
+        for _, cert_key in ipairs(ssl_cert_key) do
+          if not pl_path.exists(cert_key) then
+            errors[#errors + 1] = prefix .. "ssl_cert_key: no such file at " .. cert_key
+          end
+        end
+      end
     end
   end
 
@@ -787,25 +821,6 @@ local function check_and_infer(conf, opts)
     if conf.client_ssl_cert_key and not pl_path.exists(conf.client_ssl_cert_key) then
       errors[#errors + 1] = "client_ssl_cert_key: no such file at " ..
                           conf.client_ssl_cert_key
-    end
-  end
-
-  if (concat(conf.admin_listen, ",") .. " "):find("%sssl[%s,]") then
-    if conf.admin_ssl_cert and not conf.admin_ssl_cert_key then
-      errors[#errors + 1] = "admin_ssl_cert_key must be specified"
-
-    elseif conf.admin_ssl_cert_key and not conf.admin_ssl_cert then
-      errors[#errors + 1] = "admin_ssl_cert must be specified"
-    end
-
-    if conf.admin_ssl_cert and not pl_path.exists(conf.admin_ssl_cert) then
-      errors[#errors + 1] = "admin_ssl_cert: no such file at " ..
-                          conf.admin_ssl_cert
-    end
-
-    if conf.admin_ssl_cert_key and not pl_path.exists(conf.admin_ssl_cert_key) then
-      errors[#errors + 1] = "admin_ssl_cert_key: no such file at " ..
-                          conf.admin_ssl_cert_key
     end
   end
 
@@ -1534,19 +1549,24 @@ local function load(path, custom_conf, opts)
     conf.go_plugins_dir = pl_path.abspath(conf.go_plugins_dir)
   end
 
-  if conf.ssl_cert and conf.ssl_cert_key then
-    conf.ssl_cert = pl_path.abspath(conf.ssl_cert)
-    conf.ssl_cert_key = pl_path.abspath(conf.ssl_cert_key)
+  for _, prefix in ipairs({ "", "admin_", "status_" }) do
+    local ssl_cert = conf[prefix .. "ssl_cert"]
+    local ssl_cert_key = conf[prefix .. "ssl_cert_key"]
+
+    if ssl_cert and ssl_cert_key then
+      for i, cert in ipairs(ssl_cert) do
+        ssl_cert[i] = pl_path.abspath(cert)
+      end
+
+      for i, key in ipairs(ssl_cert_key) do
+        ssl_cert_key[i] = pl_path.abspath(key)
+      end
+    end
   end
 
   if conf.client_ssl_cert and conf.client_ssl_cert_key then
     conf.client_ssl_cert = pl_path.abspath(conf.client_ssl_cert)
     conf.client_ssl_cert_key = pl_path.abspath(conf.client_ssl_cert_key)
-  end
-
-  if conf.admin_ssl_cert and conf.admin_ssl_cert_key then
-    conf.admin_ssl_cert = pl_path.abspath(conf.admin_ssl_cert)
-    conf.admin_ssl_cert_key = pl_path.abspath(conf.admin_ssl_cert_key)
   end
 
   if conf.lua_ssl_trusted_certificate
